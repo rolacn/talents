652938edc84f9a3459dbbbaefda21b6289bb299a
rolacn <rolacn@outlook.com>
2018-08-01 09:22:29 +0800

    Hello

--- src/main/java/cn/inctech/app/StartApp.java
+++ src/main/java/cn/inctech/app/StartApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TalentsApplication {
+public class StartApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TalentsApplication.class, args);
+		SpringApplication.run(StartApp.class, args);
 	}
 }
